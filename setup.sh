a411050e777e26818a75e6d86ed9aa61ba974db6
thekillers123 <azriff456@gmail.com>
2021-09-05 13:49:02 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+echo "You need to run this script as root"
+exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+#echo "Checking VPS"
+echo -e "${green}Permintaan Diterima...${NC}"
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+echo "Please Contact Admin"
+echo "Telegram t.me/kotak16"
+echo "WhatsApp wa.me/628976118568"
+rm -f setup.sh
+exit 0
+mkdir /etc/v2ray
+mkdir /var/lib/crot-script;
+clear
+echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
+echo "Ketikkan Perintah addhost setelah proses instalasi Script Selesai"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
+echo "$host" >> /etc/v2ray/domain
+#wget https://github.com/${GitUser}/
+GitUser="rezakojexs"
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/websocket-python/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/shadowsocksobfs.sh && chmod +x shadowsocksobfs.sh && screen -S ss ./shadowsocksobfs.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/${GitUser}/test1/main/install/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+rm -f /root/ssh-vpn.sh
+rm -f /root/websocket.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ssr.sh
+rm -f /root/shadowsocksobfs.sh
+rm -f /root/ins-vt.sh
+rm -f /root/go.sh
+rm -f /root/ipsec.sh
+history -c
+echo "1.1" > /home/ver
+clear
+echo " "
+echo "Installation has been completed!!"
+echo " "
+echo "=================================-Script Mod Mr. ZEXS-===========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "----------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                  : 22, 500"  | tee -a log-install.txt
+echo "   - SSH-WS CDN OpenSSH       : 2095"  | tee -a log-install.txt
+echo "   - SSH-WS CDN Dropbear      : 8880"  | tee -a log-install.txt
+echo "   - SSH-WS CDN SSL/TLS       : 443"  | tee -a log-install.txt
+echo "   - OpenVPN-WS               : 2082"  | tee -a log-install.txt
+echo "   - OpenVPN                  : TCP 1194, UDP 2200, SSL 992, X1197"  | tee -a log-install.txt
+echo "   - Stunnel4 SSL/TLS         : 444"  | tee -a log-install.txt
+echo "   - Dropbear                 : 143, 109"  | tee -a log-install.txt
+echo "   - Squid Proxy              : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                   : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                    : 81"  | tee -a log-install.txt
+echo "   - Wireguard                : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN           : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                 : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                 : 5555"  | tee -a log-install.txt
+echo "   - Shadowsocks-R            : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS              : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP             : 3443-3453"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS          : 4443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS     : 80"  | tee -a log-install.txt
+echo "   - Trojan                   : 6443"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 00.00 GMT +7" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "------------------------------------------Mod by Kojexs------------------------------------------" | tee -a log-install.txt
+echo ""
+echo " Reboot 10 Sec"
+sleep 10
+rm -f setup.sh
+reboot
