c98d942908f48a2af284ce2b4f999ae45b194ba8
thekillers123 <azriff456@gmail.com>
2021-11-30 06:08:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/kenzotelco/ip-sahaja/main/ipvps| grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/thekillers123/AutoScriptDebian10/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
-echo -e "${green}Permission Denied!${NC}";
+echo -e "${red}Permission Denied!${NC}";
 echo "Please Contact Admin"
 echo "Telegram t.me/sampiiiiu"
 rm -f setup.sh
